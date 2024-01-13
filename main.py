8dafb910af0fcb21f923ef2391e60bf122cdae3d
Polina <blinovapolina06@yandex.ru>
2024-01-14 04:53:10 +1000

    added main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+
+
+def main():
+    pygame.init()
+    screen_width = 1200
+    screen_height = 700
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+
+    running = True
+    fps = 60
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill('black')
+
+        pygame.display.update()
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+if __name__ == '__main__':
+    main()
