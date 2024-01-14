163d9f09668d553a11154b90c6fde1a4e3894182
Polina <blinovapolina06@yandex.ru>
2024-01-14 16:47:15 +1000

    added tiles.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import *
+from tiles import Tile
 
 
 def main():
---
+++ tiles.py
@@ -0,0 +1,9 @@
+import pygame
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, position, size):
+        super(Tile, self).__init__()
+        self.image = pygame.Surface((size, size))
+        self.image.fill('green')
+        self.rect = self.image.get_rect(topleft=position)
