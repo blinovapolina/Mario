c63ccf036c4c6cb6d85c2b9c64fb64671de4dace
Polina <blinovapolina06@yandex.ru>
2024-01-14 21:55:27 +1000

    add_gravity player.py

--- player.py
+++ player.py
@@ -7,8 +7,11 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
+
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
 
     def get_input(self):
         buttons = pygame.key.get_pressed()
@@ -20,6 +23,11 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+    def add_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
+        self.add_gravity()
