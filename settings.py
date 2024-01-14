2dc1dadc19870f5908425f61e1dfc7f8991991f6
Polina <blinovapolina06@yandex.ru>
2024-01-14 13:33:01 +1000

    added level_map to settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX P                       ',
+    ' XXXX         XX         XX ',
+    ' XXXX       XX              ',
+    ' XX    X  XXXX    XX  XX    ',
+    '       X  XXXX    XX  XXX   ',
+    '    XXXX  XXXXXX  XX  XXXX  ',
+    'XXXXXXXX  XXXXXX  XX  XXXX  '
+]
+
